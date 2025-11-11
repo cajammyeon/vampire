fb5fad15069d4e19d8b3efa6885aa7b0111b3ea8
cajammyeon <syahrulazimazmi@gmail.com>
2025-11-11 12:31:24 +0000

    formatting for spinlattice energy

--- src/spinlattice/energy.cpp
+++ src/spinlattice/energy.cpp
@@ -15,122 +15,99 @@
 
 // Vampire headers
 #include "sld.hpp"
-#include <iostream>
-#include <vector>
-#include <math.h>
 #include "atoms.hpp"
 #include "create.hpp"
 #include "constants.hpp"
 #include "material.hpp"
-
-
-
-
-
-
-// sld module headers
 #include "internal.hpp"
 
+// CPP headers
+#include <iostream>
+#include <vector>
+#include <math.h>
 
 namespace sld{
 
-   double compute_potential_energy(const int start_index, // first atom for exchange interactions to be calculated
-               const int end_index,
-               const std::vector<int>& type_array){
+	double compute_potential_energy(const int start_index, 
+									const int end_index,
+									const std::vector<int>& type_array) 
+	{
+		double pot = 0.0;
+		for (int at = start_index; at < end_index; at++){
+			pot += sld::internal::potential_eng[at];
+		}
 
-//
-                double pot=0.0;
-                for (int at=start_index;at<end_index;at++){
-                    pot += sld::internal::potential_eng[at];
-                }
+		pot /= (end_index-start_index);
+		return pot;
+	}
 
-                pot /= (end_index-start_index);
-            //   std::cout<<"spin temp param "<<mp::material[0].mu_s_SI /constants::kB <<"\t"<<mp::material[0].mu_s_SI<<"\t"<<constants::kB<<std::endl;
-      return pot;
+	double compute_kinetic_energy(const int start_index,
+								  const int end_index,
+								  const std::vector<int>& type_array, 
+								  std::vector<double>& velo_array_x, 
+								  std::vector<double>& velo_array_y,
+								  std::vector<double>& velo_array_z)
+	{
+		double kinetic = 0;
+		for (int at = start_index; at < end_index; at++){
+			double vx = atoms::x_velo_array[at];
+			double vy = atoms::y_velo_array[at];
+			double vz = atoms::z_velo_array[at];
+			const unsigned int imat = atoms::type_array[at];
+			
+			kinetic += sld::internal::mp[imat].mass.get() * ((vx*vx) + (vy*vy) + (vz*vz));
+		}
 
-   }//end of potential energy
-//
-double compute_kinetic_energy(const int start_index, // first atom for exchange interactions to be calculated
-            const int end_index,
-            const std::vector<int>& type_array, // type for atom
-            std::vector<double>& velo_array_x, // coord vectors for atoms
-            std::vector<double>& velo_array_y,
-            std::vector<double>& velo_array_z){
+		kinetic *= 0.5 / (end_index-start_index);
+		return kinetic;
+	}
 
-//
-            double kinetic=0;
-             for (int at=start_index;at<end_index;at++){
-                 double vx = atoms::x_velo_array[at];
-                 double vy = atoms::y_velo_array[at];
-                 double vz = atoms::z_velo_array[at];
-                 const unsigned int imat = atoms::type_array[at];
-                
-                 kinetic+=sld::internal::mp[imat].mass.get()*(vx*vx+vy*vy+vz*vz);
-             }
+	double compute_effective_J(const int start_index,
+							   const int end_index,
+							   std::vector<double>& sumJ)
+	{
+		double sum1J = 0.0;
+		for (int at = start_index; at < end_index; at++){
+			sum1J += sumJ[at];
+		}
+		sum1J /= (end_index-start_index);
+		return sum1J;
+	}
 
-            kinetic*=0.5/(end_index-start_index);
-           // std::cout<<"kinetic en "<<kinetic<<std::endl;
+	double compute_effective_C(const int start_index,
+							   const int end_index,
+							   std::vector<double>& sumC)
+	{
+		double sum1C = 0.0;
+		for (int at = start_index; at < end_index; at++){
+			sum1C += sumC[at];
+		}
+		sum1C /= (end_index-start_index);
+		return sum1C;
+	}
 
-   return kinetic;
+	double compute_coupling_energy(const int start_index, 
+								   const int end_index)
+	{
+		double sumC = 0.0;
+		for (int at = start_index; at < end_index; at++){
+			sumC += sld::internal::coupl_eng[at];
+		}
+		sumC /= (end_index-start_index);
+		sumC *= mp::material[0].mu_s_SI / 1.602176634e-19;
+		return sumC;
+	}
 
-}//end of kinetic energy
+	double compute_exchange_energy(const int start_index,
+								   const int end_index)
+	{
+		double sumJ = 0.0;
+		for (int at = start_index; at < end_index; at++){
+			sumJ += sld::internal::exch_eng[at];
+		}
+		sumJ /= (end_index-start_index);
+		sumJ *= mp::material[0].mu_s_SI / 1.602176634e-19;
+		return sumJ;
+	}
 
-double compute_effective_J(const int start_index, // first atom for exchange interactions to be calculated
-            const int end_index,
-            std::vector<double>& sumJ){
-
-             double sum1J=0.0;
-             for (int at=start_index;at<end_index;at++){
-                 sum1J += sumJ[at];
-             }
-             sum1J /= (end_index-start_index);
-   return sum1J;
-
-}//end of compute eff exchange
-
-double compute_effective_C(const int start_index, // first atom for exchange interactions to be calculated
-            const int end_index,
-            std::vector<double>& sumC){
-
-             double sum1C=0.0;
-             for (int at=start_index;at<end_index;at++){
-                 sum1C += sumC[at];
-             }
-             sum1C /= (end_index-start_index);
-   return sum1C;
-
-}//end of compute eff coupling
-
-double compute_coupling_energy(const int start_index, // first atom for exchange interactions to be calculated
-            const int end_index){
-
-
-             double sumC=0.0;
-             for (int at=start_index;at<end_index;at++){
-                 sumC += sld::internal::coupl_eng[at];
-             }
-
-             sumC /= (end_index-start_index);
-             sumC *= mp::material[0].mu_s_SI/1.602176634e-19; //in eV at the moment
-
-   return sumC;
-
-}//end of potential energy
-
-double compute_exchange_energy(const int start_index, // first atom for exchange interactions to be calculated
-            const int end_index){
-
-//
-             double sumJ=0.0;
-             for (int at=start_index;at<end_index;at++){
-                 sumJ += sld::internal::exch_eng[at];
-             }
-
-             sumJ /= (end_index-start_index);
-             sumJ *= mp::material[0].mu_s_SI/1.602176634e-19;//in eV at the moment
-   return sumJ;
-
-}//end of potential energy
-
-
-   } // end of sld namespace
+} 
