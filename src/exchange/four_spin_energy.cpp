0525cbcf3fac02b2cd29e70585dc1e08e647ec9e
cajammyeon <syahrulazimazmi@gmail.com>
2025-12-01 16:57:23 +0000

    formatting for fourspin

--- src/exchange/four_spin_energy.cpp
+++ src/exchange/four_spin_energy.cpp
@@ -13,18 +13,20 @@
 // C++ standard library headers
 
 // Vampire headers
-#include "atoms.hpp" // for exchange list type defs
+#include "atoms.hpp" 
 #include "exchange.hpp"
 
 // exchange module headers
 #include "internal.hpp"
 
-double dot_product2(double six, double siy, double siz, double sjx, double sjy, double sjz){
-   double dp = six*sjx + siy*sjy + siz*sjz;
-   return dp;
+double dot_product2(double six, double siy, double siz, double sjx, double sjy, double sjz)
+{
+	double dp = six*sjx + siy*sjy + siz*sjz;
+	return dp;
 }
 
-namespace exchange{
+namespace exchange
+{
 
 //-----------------------------------------------------------------------------------------
 // Function to calculate Four Spin exchange fields for spins between start and end index
@@ -40,48 +42,47 @@ namespace exchange{
 //    H_4s^z = -dE/dSiz =1/3 Dq (Szj (Sk . Sl) + Szk (Sl . Sj) + Szl (Sk . Sj))
 //-----------------------------------------------------------------------------------------
 
-double single_spin_four_spin_energy(const int atom, const double sx, const double sy, const double sz){
+	double single_spin_four_spin_energy(const int atom, const double sx, const double sy, const double sz)
+	{
 
-   if (!internal::enable_fourspin) return 0.0;
+		if (!internal::enable_fourspin) return 0.0;
 
-   double energy=0.0;
+		double energy=0.0;
 
-   const double six = atoms::x_spin_array[atom];
-   const double siy = atoms::y_spin_array[atom];
-   const double siz = atoms::z_spin_array[atom];
+		const double six = atoms::x_spin_array[atom];
+		const double siy = atoms::y_spin_array[atom];
+		const double siz = atoms::z_spin_array[atom];
 
-   // Loop over neighbouring spins to calculate exchange
-   for(int nn = internal::four_spin_neighbour_list_start_index[atom]; nn <= internal::four_spin_neighbour_list_end_index[atom]; ++nn){
+		// Loop over neighbouring spins to calculate exchange
+		for(int nn = internal::four_spin_neighbour_list_start_index[atom]; nn <= internal::four_spin_neighbour_list_end_index[atom]; ++nn)
+		{
+			const int natomj = internal::four_spin_neighbour_list_array_j[nn];
+			const int natomk = internal::four_spin_neighbour_list_array_k[nn];
+			const int natoml = internal::four_spin_neighbour_list_array_l[nn];
 
-      const int natomj = internal::four_spin_neighbour_list_array_j[nn];
-		const int natomk = internal::four_spin_neighbour_list_array_k[nn];
-		const int natoml = internal::four_spin_neighbour_list_array_l[nn];
+			const double sjx = atoms::x_spin_array[natomj];
+			const double sjy = atoms::y_spin_array[natomj];
+			const double sjz = atoms::z_spin_array[natomj];
 
-      const double sjx = atoms::x_spin_array[natomj];
-      const double sjy = atoms::y_spin_array[natomj];
-      const double sjz = atoms::z_spin_array[natomj];
+			const double skx = atoms::x_spin_array[natomk];
+			const double sky = atoms::y_spin_array[natomk];
+			const double skz = atoms::z_spin_array[natomk];
 
-      const double skx = atoms::x_spin_array[natomk];
-      const double sky = atoms::y_spin_array[natomk];
-      const double skz = atoms::z_spin_array[natomk];
+			const double slx = atoms::x_spin_array[natoml];
+			const double sly = atoms::y_spin_array[natoml];
+			const double slz = atoms::z_spin_array[natoml];
 
-      const double slx = atoms::x_spin_array[natoml];
-      const double sly = atoms::y_spin_array[natoml];
-      const double slz = atoms::z_spin_array[natoml];
+			const double si_dot_sj = dot_product2(six,siy,siz,sjx,sjy,sjz);
+			const double si_dot_sk = dot_product2(six,siy,siz,skx,sky,skz);
+			const double si_dot_sl = dot_product2(six,siy,siz,slx,sly,slz);
+			const double sk_dot_sl = dot_product2(skx,sky,skz,slx,sly,slz);
+			const double sj_dot_sk = dot_product2(skx,sky,skz,sjx,sjy,sjz);
+			const double sj_dot_sl = dot_product2(sjx,sjy,sjz,slx,sly,slz);
+			const double Jij = internal::four_spin_exchange_list[nn];
 
-      const double si_dot_sj = dot_product2(six,siy,siz,sjx,sjy,sjz);
-      const double si_dot_sk = dot_product2(six,siy,siz,skx,sky,skz);
-      const double si_dot_sl = dot_product2(six,siy,siz,slx,sly,slz);
-      const double sk_dot_sl = dot_product2(skx,sky,skz,slx,sly,slz);
-      const double sj_dot_sk = dot_product2(skx,sky,skz,sjx,sjy,sjz);
-      const double sj_dot_sl = dot_product2(sjx,sjy,sjz,slx,sly,slz);
-      const double Jij = internal::four_spin_exchange_list[nn];
+			energy = energy - 4.0*Jij/12.0*(si_dot_sj*sk_dot_sl + si_dot_sk*sj_dot_sl + si_dot_sl*sj_dot_sk);
+		}
 
-      energy = energy - 4.0*Jij/12.0*(si_dot_sj*sk_dot_sl + si_dot_sk*sj_dot_sl + si_dot_sl*sj_dot_sk);
-
-    }
-
-    return 0;
+		return 0;
+	}
 }
-
-} // end of namespace
