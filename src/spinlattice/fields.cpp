8c29fa2bbc27546d62e536822079b95d990ecf37
cajammyeon <syahrulazimazmi@gmail.com>
2025-12-02 15:39:04 +0000

    formatting

--- src/spinlattice/fields.cpp
+++ src/spinlattice/fields.cpp
@@ -136,7 +136,7 @@ namespace sld
 	      	double exch_inv_rcut = 1.0/sld::internal::r_cut_fields;
        		double sumJ = 0.0;
 
-       		for(int i=start_index;i<end_index; ++i)
+       		for(int i = start_index; i < end_index; ++i)
 			{
 
           		const unsigned int imat = atoms::type_array[i];
@@ -150,9 +150,11 @@ namespace sld
 				fx = 0.0;
 				fy = 0.0;
 				fz = 0.0;
+
 				hx = 0.0;
 				hy = 0.0;
 				hz = 0.0;
+
 				sumJ = 0.0;
 				energy = 0.0;
 
@@ -164,7 +166,7 @@ namespace sld
 				sy = y_spin_array[i];
 				sz = z_spin_array[i];
 
-          		for( int n = nbr_start; n < nbr_end; ++n)
+          		for(int n = nbr_start; n < nbr_end; ++n)
 				{
 
             		j = neighbour_list_array[n];
@@ -179,7 +181,6 @@ namespace sld
 						dy = sld::PBC_wrap(dy, cs::system_dimensions[1], cs::pbc[1]);
 						dz = sld::PBC_wrap(dz, cs::system_dimensions[2], cs::pbc[2]);
 
-
 						rji_sqr = (dx*dx) + (dy*dy) + (dz*dz);
 
              			if(rji_sqr < r_sqr_cut)
@@ -189,7 +190,7 @@ namespace sld
 							double J_prime, J_init;
 
                  			rji = sqrt(rji_sqr);
-                 			inv_rji = 1.0/ rji;
+                 			inv_rji = 1.0 / rji;
 
                  			// y = (-1.6013789004079091e-18) + (1.1923770275429103e-18) * (x ** 1) + (2.050929317476752e-20) * (x ** 2) + 
 							// (-2.9896826732643693e-19) * (x ** 3) + (1.357513552544028e-19) * (x ** 4) + (-3.03650431347386e-20) * (x ** 5) + 
@@ -226,9 +227,9 @@ namespace sld
 							sjz = z_spin_array[j];
 							
 							// Field calculation - component
-							hx += J * sjx ;
-							hy += J * sjy ;
-							hz += J * sjz ;
+							hx += (J * sjx);
+							hy += (J * sjy);
+							hz += (J * sjz);
 							sumJ += J;
 
 							// (S_i . S_j)
@@ -252,7 +253,7 @@ namespace sld
 							fy += (J_prime * si_dot_sj * dy * inv_rji);
 							fz += (J_prime * si_dot_sj * dz * inv_rji);
 
-							energy += J *  (si_dot_sj);
+							energy += (J * si_dot_sj);
              			}
           			}
        			}
@@ -297,14 +298,16 @@ namespace sld
 			double sx, sy, sz;
 			double sjx, sjy,sjz;
 			double si_dot_sj;
+
 			double fc_x = 0.0, fc_y = 0.0, fc_z = 0.0;
 			double hc_x = 0.0, hc_y = 0.0, hc_z = 0.0;
+
 			double rji_sqr, rji, inv_rji,  inv_rji2, inv_rji4, inv_rji6;
 			double sj_dot_rji, si_dot_rji;
 			double energy_c;
 			int j; 
 
-			double r_sqr_cut=sld::internal::r_cut_fields*sld::internal::r_cut_fields;
+			double r_sqr_cut = sld::internal::r_cut_fields * sld::internal::r_cut_fields;
 			double oneover3 = 1.0/3.0;
 			double sumC;
 
@@ -312,11 +315,11 @@ namespace sld
 			{
 
 				const unsigned int imat = atoms::type_array[i];
-				double fact=sld::internal::mp[imat].C0.get()/1.602176634e-19;
-				double fact_ms= sld::internal::mp[imat].C0_ms.get();
+				double fact =sld::internal::mp[imat].C0.get()/1.602176634e-19;
+				double fact_ms = sld::internal::mp[imat].C0_ms.get();
 
 				int nbr_start = neighbour_list_start_index[i];
-				int nbr_end = neighbour_list_end_index[i]+1;
+				int nbr_end = neighbour_list_end_index[i] + 1;
 
 				fc_x = 0.0;
 				fc_y = 0.0;
@@ -324,8 +327,8 @@ namespace sld
 				hc_x = 0.0;
 				hc_y = 0.0;
 				hc_z = 0.0;
-				energy_c=0.0;
-				sumC=0.0;
+				energy_c = 0.0;
+				sumC = 0.0;
 
 				rx = x_coord_array[i];
 				ry = y_coord_array[i];
@@ -346,43 +349,39 @@ namespace sld
 						dy = -y_coord_array[j] + ry;
 						dz = -z_coord_array[j] + rz;
 
-						dx = sld::PBC_wrap( dx, cs::system_dimensions[0], cs::pbc[0]);
-						dy = sld::PBC_wrap( dy, cs::system_dimensions[1], cs::pbc[1]);
-						dz = sld::PBC_wrap( dz, cs::system_dimensions[2], cs::pbc[2]);
+						dx = sld::PBC_wrap(dx, cs::system_dimensions[0], cs::pbc[0]);
+						dy = sld::PBC_wrap(dy, cs::system_dimensions[1], cs::pbc[1]);
+						dz = sld::PBC_wrap(dz, cs::system_dimensions[2], cs::pbc[2]);
 
-
-						rji_sqr = dx*dx + dy*dy + dz*dz;
+						rji_sqr = (dx*dx) + (dy*dy) + (dz*dz);
 
 						if(rji_sqr < r_sqr_cut)
 						{
-
                             rji = sqrt(rji_sqr);
-                            inv_rji = 1.0/ rji;
+                            inv_rji = 1.0 / rji;
 
                             sjx = x_spin_array[j];
                             sjy = y_spin_array[j];
                             sjz = z_spin_array[j];
 
-                            si_dot_sj = sx * sjx + sy * sjy + sz * sjz;
+                            si_dot_sj = (sx * sjx) + (sy * sjy) + (sz * sjz);
 
-                            sj_dot_rji = (dx * sjx + dy * sjy + dz * sjz);
-                            si_dot_rji = (dx * sx  + dy * sy  + dz * sz);
+                            sj_dot_rji = (dx * sjx) + (dy * sjy) + (dz * sjz);
+                            si_dot_rji = (dx * sx)  + (dy * sy)  + (dz * sz);
 
-                            inv_rji2=inv_rji*inv_rji;
-                            inv_rji4=inv_rji2*inv_rji2;
-                            inv_rji6=inv_rji4*inv_rji2;
+                            inv_rji2 = inv_rji * inv_rji;
+                            inv_rji4 = inv_rji2 * inv_rji2;
+                            inv_rji6 = inv_rji4 * inv_rji2;
 
-                            hc_x +=fact_ms*inv_rji4*(inv_rji2*dx*sj_dot_rji-oneover3*sjx);
-                            hc_y +=fact_ms*inv_rji4*(inv_rji2*dy*sj_dot_rji-oneover3*sjy);
-                            hc_z +=fact_ms*inv_rji4*(inv_rji2*dz*sj_dot_rji-oneover3*sjz);
+                            hc_x += (fact_ms * inv_rji4 * (inv_rji2 * dx * sj_dot_rji - (oneover3 * sjx)));
+                            hc_y += (fact_ms * inv_rji4 * (inv_rji2 * dy * sj_dot_rji - (oneover3 * sjy)));
+                            hc_z += (fact_ms * inv_rji4 * (inv_rji2 * dz * sj_dot_rji - (oneover3 * sjz)));
 
-							fc_x += fact*inv_rji6*( sj_dot_rji * sx + si_dot_rji * sjx -6.0* dx* sj_dot_rji* si_dot_rji * inv_rji2+ oneover3*4.0*si_dot_sj*dx);
-                            fc_y += fact*inv_rji6*( sj_dot_rji * sy + si_dot_rji * sjy -6.0* dy* sj_dot_rji* si_dot_rji * inv_rji2+ oneover3*4.0*si_dot_sj*dy);
-                            fc_z += fact*inv_rji6*( sj_dot_rji * sz + si_dot_rji * sjz -6.0* dz* sj_dot_rji* si_dot_rji * inv_rji2+ oneover3*4.0*si_dot_sj*dz);
+							fc_x += (fact * inv_rji6 * ((sj_dot_rji * sx) + (si_dot_rji * sjx) - (6.0 * dx * sj_dot_rji * si_dot_rji * inv_rji2) + (oneover3 * 4.0 * si_dot_sj * dx)));
+                            fc_y += (fact * inv_rji6 * ((sj_dot_rji * sy) + (si_dot_rji * sjy) - (6.0 * dy * sj_dot_rji * si_dot_rji * inv_rji2) + (oneover3 * 4.0 * si_dot_sj * dy)));
+                            fc_z += (fact * inv_rji6 * ((sj_dot_rji * sz) + (si_dot_rji * sjz) - (6.0 * dz * sj_dot_rji * si_dot_rji * inv_rji2) + (oneover3 * 4.0 * si_dot_sj * dz)));
 
-
-
-                            sumC +=fact_ms*inv_rji4;
+                            sumC += (fact_ms * inv_rji4);
                   		}
                		}
             	}
@@ -395,8 +394,8 @@ namespace sld
 				fields_array_y[i] += hc_y;
 				fields_array_z[i] += hc_z;
 
-				sld::internal::sumC[i]=sumC;
-				sld::internal::coupl_eng[i]=-0.5*energy_c;
+				sld::internal::sumC[i] = sumC;
+				sld::internal::coupl_eng[i] = -0.5 * energy_c;
 
 			}
             return;
@@ -427,21 +426,23 @@ namespace sld
 			double sx, sy, sz;
 			double sjx, sjy,sjz;
 			double si_dot_sj;
+
 			double fc_x = 0.0, fc_y = 0.0, fc_z = 0.0;
 			double hc_x = 0.0, hc_y = 0.0, hc_z = 0.0;
+
 			double rji_sqr, rji, inv_rji,  inv_rji2, inv_rji4, inv_rji6;
 			double sj_dot_rji, si_dot_rji;
 			double energy_c;
 			int j; 
 
-			double r_sqr_cut=sld::internal::r_cut_fields*sld::internal::r_cut_fields;
-			double oneover3=1.0/3.0;
+			double r_sqr_cut = sld::internal::r_cut_fields * sld::internal::r_cut_fields;
+			double oneover3 = 1.0 / 3.0;
 
 			for(int i=start_index;i<end_index; ++i)
 			{
 				const unsigned int imat = atoms::type_array[i];
-				double fact=sld::internal::mp[imat].C0.get()/1.602176634e-19;
-				double fact_ms= sld::internal::mp[imat].C0_ms.get();
+				double fact = sld::internal::mp[imat].C0.get() / 1.602176634e-19;
+				double fact_ms = sld::internal::mp[imat].C0_ms.get();
 
 				fc_x = 0.0;
 				fc_y = 0.0;
@@ -449,7 +450,7 @@ namespace sld
 				hc_x = 0.0;
 				hc_y = 0.0;
 				hc_z = 0.0;
-				energy_c=0.0;
+				energy_c = 0.0;
 
 				rx = x_coord_array[i];
 				ry = y_coord_array[i];
@@ -460,27 +461,24 @@ namespace sld
 				sz = z_spin_array[i];
 
 				int nbr_start = neighbour_list_start_index[i];
-				int nbr_end = neighbour_list_end_index[i]+1;
-
+				int nbr_end = neighbour_list_end_index[i] + 1;
 
                 for( int n = nbr_start; n < nbr_end; ++n)
 				{
 					j = neighbour_list_array[n];
 
-
-					if ( j != i)
+					if (j != i)
 					{
 
 						dx = -x_coord_array[j] + rx;
 						dy = -y_coord_array[j] + ry;
 						dz = -z_coord_array[j] + rz;
 
-						dx = sld::PBC_wrap( dx, cs::system_dimensions[0], cs::pbc[0]);
-						dy = sld::PBC_wrap( dy, cs::system_dimensions[1], cs::pbc[1]);
-						dz = sld::PBC_wrap( dz, cs::system_dimensions[2], cs::pbc[2]);
+						dx = sld::PBC_wrap(dx, cs::system_dimensions[0], cs::pbc[0]);
+						dy = sld::PBC_wrap(dy, cs::system_dimensions[1], cs::pbc[1]);
+						dz = sld::PBC_wrap(dz, cs::system_dimensions[2], cs::pbc[2]);
 
-
-						rji_sqr = dx*dx + dy*dy + dz*dz;
+						rji_sqr = (dx * dx) + (dy * dy) + (dz * dz);
 
 						if(rji_sqr < r_sqr_cut)
 						{
@@ -491,55 +489,54 @@ namespace sld
 							sjy = y_spin_array[j];
 							sjz = z_spin_array[j];
 
-							si_dot_sj = sx * sjx + sy * sjy + sz * sjz;
+							si_dot_sj = (sx * sjx) + (sy * sjy) + (sz * sjz);
 
-							sj_dot_rji = (dx * sjx + dy * sjy + dz * sjz);
-							si_dot_rji = (dx * sx  + dy * sy  + dz * sz);
+							sj_dot_rji = (dx * sjx) + (dy * sjy) + (dz * sjz);
+							si_dot_rji = (dx * sx)  + (dy * sy)  + (dz * sz);
 
-							inv_rji2=inv_rji*inv_rji;
-							inv_rji4=inv_rji2*inv_rji2;
-							inv_rji6=inv_rji4*inv_rji2;
+							inv_rji2 = inv_rji * inv_rji;
+							inv_rji4 = inv_rji2 * inv_rji2;
+							inv_rji6 = inv_rji4 * inv_rji2;
 
-							double prod1=inv_rji2*si_dot_rji*si_dot_rji- oneover3*si_dot_sj;
-							double prod2=inv_rji2*sj_dot_rji*sj_dot_rji- oneover3*si_dot_sj;
-							double prod3=si_dot_rji*sj_dot_rji*sj_dot_rji*sj_dot_rji;
-							double prod4=sj_dot_rji*si_dot_rji*si_dot_rji*si_dot_rji;
-							double sj3=sj_dot_rji*sj_dot_rji*sj_dot_rji;
-							double si3=si_dot_rji*si_dot_rji*si_dot_rji;
-							double deriv1=2*inv_rji2*si_dot_rji;
+							double prod1 = (inv_rji2 * si_dot_rji * si_dot_rji) - (oneover3 * si_dot_sj);
+							double prod2 = (inv_rji2 * sj_dot_rji * sj_dot_rji) - (oneover3 * si_dot_sj);
+							double prod3 = si_dot_rji * sj_dot_rji * sj_dot_rji * sj_dot_rji;
+							double prod4 = sj_dot_rji * si_dot_rji * si_dot_rji * si_dot_rji;
+							double sj3 = sj_dot_rji * sj_dot_rji * sj_dot_rji;
+							double si3 = si_dot_rji * si_dot_rji * si_dot_rji;
+							double deriv1 = 2 * inv_rji2 * si_dot_rji;
+							
+							// Field calculation
+                            hc_x += ((12.0/35.0) * fact_ms * inv_rji4 * ((inv_rji2 * dx * sj_dot_rji) - (oneover3 * sjx)));
+                            hc_y += ((12.0/35.0) * fact_ms * inv_rji4 * ((inv_rji2 * dy * sj_dot_rji) - (oneover3 * sjy)));
+                            hc_z += ((12.0/35.0) * fact_ms * inv_rji4 * ((inv_rji2 * dz * sj_dot_rji) - (oneover3 * sjz)));
 
+                            energy_c += ((12.0/35.0) * fact_ms * inv_rji4 * ((inv_rji2 * sj_dot_rji * si_dot_rji) - (oneover3 * si_dot_sj)));
 
+                            hc_x += ((9.0/5.0) * fact_ms * inv_rji4 * ((((deriv1 * dx) - (oneover3 * sjx)) * prod2) + (prod1 * (-oneover3 * sjx))));
+                            hc_y += ((9.0/5.0) * fact_ms * inv_rji4 * ((((deriv1 * dy) - (oneover3 * sjy)) * prod2) + (prod1 * (-oneover3 * sjy))));
+                            hc_z += ((9.0/5.0) * fact_ms * inv_rji4 * ((((deriv1 * dz) - (oneover3 * sjz)) * prod2) + (prod1 * (-oneover3 * sjz))));
 
-                            hc_x +=12.0/35.0*fact_ms*inv_rji4*(inv_rji2*dx*sj_dot_rji-oneover3*sjx);
-                            hc_y +=12.0/35.0*fact_ms*inv_rji4*(inv_rji2*dy*sj_dot_rji-oneover3*sjy);
-                            hc_z +=12.0/35.0*fact_ms*inv_rji4*(inv_rji2*dz*sj_dot_rji-oneover3*sjz);
+                            energy_c += ((9.0/5.0) * fact_ms * inv_rji4 * prod1 * prod2);
 
-                            energy_c+= 12.0/35.0*fact_ms*inv_rji4*(inv_rji2*sj_dot_rji*si_dot_rji- oneover3*si_dot_sj);
+							hc_x += ((-2.0/5.0) * fact_ms * inv_rji4 * inv_rji4 * ((dx * sj3) + (3.0 * dx * sj_dot_rji * si_dot_rji * si_dot_rji)));
+                            hc_y += ((-2.0/5.0) * fact_ms * inv_rji4 * inv_rji4 * ((dy * sj3) + (3.0 * dy * sj_dot_rji * si_dot_rji * si_dot_rji)));
+                            hc_z += ((-2.0/5.0) * fact_ms * inv_rji4 * inv_rji4 * ((dz * sj3) + (3.0 * dz * sj_dot_rji * si_dot_rji * si_dot_rji)));
 
-                            hc_x +=9.0/5.0*fact_ms*inv_rji4*((deriv1*dx-oneover3*sjx)*prod2+prod1*(-oneover3*sjx));
-                            hc_y +=9.0/5.0*fact_ms*inv_rji4*((deriv1*dy-oneover3*sjy)*prod2+prod1*(-oneover3*sjy));
-                            hc_z +=9.0/5.0*fact_ms*inv_rji4*((deriv1*dz-oneover3*sjz)*prod2+prod1*(-oneover3*sjz));
+                            energy_c += ((-2.0/5.0) * fact_ms * inv_rji4 * inv_rji4 * (prod3 + prod4));
+							
+							// Force calculation
+                            fc_x += ((12.0/35.0) * fact * inv_rji6 * ((sj_dot_rji * sx + si_dot_rji * sjx) - (6.0 * dx * sj_dot_rji * si_dot_rji * inv_rji2) + (oneover3 * 4.0 * si_dot_sj * dx)));
+                            fc_y += ((12.0/35.0) * fact * inv_rji6 * ((sj_dot_rji * sy + si_dot_rji * sjy) - (6.0 * dy * sj_dot_rji * si_dot_rji * inv_rji2) + (oneover3 * 4.0 * si_dot_sj * dy)));
+                            fc_z += ((12.0/35.0) * fact * inv_rji6 * ((sj_dot_rji * sz + si_dot_rji * sjz) - (6.0 * dz * sj_dot_rji * si_dot_rji * inv_rji2) + (oneover3 * 4.0 * si_dot_sj * dz)));
 
-                            energy_c +=9.0/5.0*fact_ms*inv_rji4*prod1*prod2;
+                            fc_x += (9.0/5.0) * fact * ((-4 * dx * inv_rji6 * prod1 * prod2) + inv_rji4 * prod2 * ((2 * sx * inv_rji2 * si_dot_rji) - (2 * dx * si_dot_rji * si_dot_rji * inv_rji4)) + inv_rji4 * prod1 * (2 * sjx * inv_rji2 * sj_dot_rji - 2 * dx * sj_dot_rji * sj_dot_rji * inv_rji4));
+                            fc_y += (9.0/5.0) * fact * ((-4 * dy * inv_rji6 * prod1 * prod2) + inv_rji4 * prod2 * ((2 * sy * inv_rji2 * si_dot_rji) - (2 * dy * si_dot_rji * si_dot_rji * inv_rji4)) + inv_rji4 * prod1 * (2 * sjy * inv_rji2 * sj_dot_rji - 2 * dy * sj_dot_rji * sj_dot_rji * inv_rji4));
+                            fc_z += (9.0/5.0) * fact * ((-4 * dz * inv_rji6 * prod1 * prod2) + inv_rji4 * prod2 * ((2 * sz * inv_rji2 * si_dot_rji) - (2 * dz * si_dot_rji * si_dot_rji * inv_rji4)) + inv_rji4 * prod1 * (2 * sjz * inv_rji2 * sj_dot_rji - 2 * dz * sj_dot_rji * sj_dot_rji * inv_rji4));
 
-							hc_x +=-2.0/5.0*fact_ms*inv_rji4*inv_rji4*(dx*sj3+3.0*dx*sj_dot_rji*si_dot_rji*si_dot_rji);
-                            hc_y +=-2.0/5.0*fact_ms*inv_rji4*inv_rji4*(dy*sj3+3.0*dy*sj_dot_rji*si_dot_rji*si_dot_rji);
-                            hc_z +=-2.0/5.0*fact_ms*inv_rji4*inv_rji4*(dz*sj3+3.0*dz*sj_dot_rji*si_dot_rji*si_dot_rji);
-
-                            energy_c +=-2.0/5.0*fact_ms*inv_rji4*inv_rji4*(prod3+prod4);
-
-
-                            fc_x += 12.0/35.0*fact*inv_rji6*( sj_dot_rji * sx + si_dot_rji * sjx -6.0* dx* sj_dot_rji* si_dot_rji * inv_rji2+ oneover3*4.0*si_dot_sj*dx);
-                            fc_y += 12.0/35.0*fact*inv_rji6*( sj_dot_rji * sy + si_dot_rji * sjy -6.0* dy* sj_dot_rji* si_dot_rji * inv_rji2+ oneover3*4.0*si_dot_sj*dy);
-                            fc_z += 12.0/35.0*fact*inv_rji6*( sj_dot_rji * sz + si_dot_rji * sjz -6.0* dz* sj_dot_rji* si_dot_rji * inv_rji2+ oneover3*4.0*si_dot_sj*dz);
-
-                            fc_x += 9.0/5.0*fact*((-4*dx*inv_rji6)*prod1*prod2+ inv_rji4*prod2*(2*sx*inv_rji2*si_dot_rji-2*dx*si_dot_rji*si_dot_rji*inv_rji4) + inv_rji4*prod1*(2*sjx*inv_rji2*sj_dot_rji-2*dx*sj_dot_rji*sj_dot_rji*inv_rji4));
-                            fc_y += 9.0/5.0*fact*((-4*dy*inv_rji6)*prod1*prod2+ inv_rji4*prod2*(2*sy*inv_rji2*si_dot_rji-2*dy*si_dot_rji*si_dot_rji*inv_rji4) + inv_rji4*prod1*(2*sjy*inv_rji2*sj_dot_rji-2*dy*sj_dot_rji*sj_dot_rji*inv_rji4));
-                            fc_z += 9.0/5.0*fact*((-4*dz*inv_rji6)*prod1*prod2+ inv_rji4*prod2*(2*sz*inv_rji2*si_dot_rji-2*dz*si_dot_rji*si_dot_rji*inv_rji4) + inv_rji4*prod1*(2*sjz*inv_rji2*sj_dot_rji-2*dz*sj_dot_rji*sj_dot_rji*inv_rji4));
-
-                            fc_x += -2.0/5.0*fact*((-4*dx*inv_rji6)*(inv_rji4*prod3+inv_rji4*prod4)+ inv_rji4*(-4*dx*inv_rji6*prod3 +inv_rji4*sx*sj3+inv_rji4*si_dot_rji*3*sjx*sj_dot_rji*sj_dot_rji)+ inv_rji4*(-4*dx*inv_rji6*prod4 +inv_rji4*sjx*si3+inv_rji4*sj_dot_rji*3*sx*si_dot_rji*si_dot_rji));
-                            fc_y += -2.0/5.0*fact*((-4*dy*inv_rji6)*(inv_rji4*prod3+inv_rji4*prod4)+ inv_rji4*(-4*dy*inv_rji6*prod3 +inv_rji4*sy*sj3+inv_rji4*si_dot_rji*3*sjy*sj_dot_rji*sj_dot_rji)+ inv_rji4*(-4*dy*inv_rji6*prod4 +inv_rji4*sjy*si3+inv_rji4*sj_dot_rji*3*sy*si_dot_rji*si_dot_rji));
-                            fc_z += -2.0/5.0*fact*((-4*dz*inv_rji6)*(inv_rji4*prod3+inv_rji4*prod4)+ inv_rji4*(-4*dz*inv_rji6*prod3 +inv_rji4*sz*sj3+inv_rji4*si_dot_rji*3*sjz*sj_dot_rji*sj_dot_rji)+ inv_rji4*(-4*dz*inv_rji6*prod4 +inv_rji4*sjz*si3+inv_rji4*sj_dot_rji*3*sz*si_dot_rji*si_dot_rji));
+                            fc_x += (-2.0/5.0) * fact * ((-4 * dx * inv_rji6) * (inv_rji4 * prod3 + inv_rji4 * prod4) + inv_rji4 * (-4 * dx * inv_rji6 * prod3 + inv_rji4 * sx * sj3 + inv_rji4 * si_dot_rji * 3 * sjx * sj_dot_rji * sj_dot_rji) + inv_rji4 * (-4 * dx * inv_rji6 * prod4 + inv_rji4 * sjx * si3 + inv_rji4 * sj_dot_rji * 3 * sx * si_dot_rji * si_dot_rji));
+                            fc_y += (-2.0/5.0) * fact * ((-4 * dy * inv_rji6) * (inv_rji4 * prod3 + inv_rji4 * prod4) + inv_rji4 * (-4 * dy * inv_rji6 * prod3 + inv_rji4 * sy * sj3 + inv_rji4 * si_dot_rji * 3 * sjy * sj_dot_rji * sj_dot_rji) + inv_rji4 * (-4 * dy * inv_rji6 * prod4 + inv_rji4 * sjy * si3 + inv_rji4 * sj_dot_rji * 3 * sy * si_dot_rji * si_dot_rji));
+                            fc_z += (-2.0/5.0) * fact * ((-4 * dz * inv_rji6) * (inv_rji4 * prod3 + inv_rji4 * prod4) + inv_rji4 * (-4 * dz * inv_rji6 * prod3 + inv_rji4 * sz * sj3 + inv_rji4 * si_dot_rji * 3 * sjz * sj_dot_rji * sj_dot_rji) + inv_rji4 * (-4 * dz * inv_rji6 * prod4 + inv_rji4 * sjz * si3 + inv_rji4 * sj_dot_rji * 3 * sz * si_dot_rji * si_dot_rji));
 						}
                		}
             	}
@@ -551,8 +548,8 @@ namespace sld
 				fields_array_y[i] += hc_y;
 				fields_array_z[i] += hc_z;
 
-				sld::internal::sumC[i]=hc_x*sx+hc_y*sy+hc_z*sz;
-				sld::internal::coupl_eng[i]=-0.5*energy_c;
+				sld::internal::sumC[i] = (hc_x * sx) + (hc_y * sy) + (hc_z*sz);
+				sld::internal::coupl_eng[i] = -0.5 * energy_c;
             }
             return;
         }
