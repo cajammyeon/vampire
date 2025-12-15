7773a6c91a3fa9d44580a0ede1b91e93183ec5b0
cajammyeon <syahrulazimazmi@gmail.com>
2025-12-15 11:39:58 +0000

    big fix

--- src/spinlattice/fields.cpp
+++ src/spinlattice/fields.cpp
@@ -299,7 +299,7 @@ namespace sld
 							power_7 = (-3.043052187202497e-22)  * rji_6 * 7;
 							power_8 = (1.291292480059483e-23)   * rji_7 * 8;
 							power_9 = (-2.3323729478397598e-25) * rji_8 * 9;
-							J_prime = power_1 + power_2 + power_3 + power_4 + power_5 + power_6 + power_7 + power_8 + power_9;
+							J_prime = (power_1 + power_2 + power_3 + power_4 + power_5 + power_6 + power_7 + power_8 + power_9);
 							
 							// Normalised force on components
 							fx += (J_prime * si_dot_sj * dx * inv_rji);
